5af0c57870a13e3d2e4fd32c6f74d62643a830db
Rodrigo Meira de Andrade <rodrigormda@hotmail.com>
2018-10-21 15:06:52 -0300

    better tqdm call

--- preprocessing.py
+++ preprocessing.py
@@ -15,50 +15,48 @@ def parse_annotation_xml(ann_dir, img_dir, labels=[]):
     seen_labels = {}
     
     ann_files = os.listdir(ann_dir)
-    with tqdm(total=len(ann_files)) as pbar:
-        for ann in sorted(ann_files):
-            pbar.update(1)
-            img = {'object':[]}
+    for ann in tqdm(sorted(ann_files)):
+        img = {'object':[]}
 
-            tree = ET.parse(os.path.join(ann_dir,ann))
-            
-            for elem in tree.iter():
-                if 'filename' in elem.tag:
-                    img['filename'] = os.path.join(img_dir, elem.text)
-                if 'width' in elem.tag:
-                    img['width'] = int(elem.text)
-                if 'height' in elem.tag:
-                    img['height'] = int(elem.text)
-                if 'object' in elem.tag or 'part' in elem.tag:
-                    obj = {}
-                    
-                    for attr in list(elem):
-                        if 'name' in attr.tag:
-                            obj['name'] = attr.text
+        tree = ET.parse(os.path.join(ann_dir,ann))
+        
+        for elem in tree.iter():
+            if 'filename' in elem.tag:
+                img['filename'] = os.path.join(img_dir, elem.text)
+            if 'width' in elem.tag:
+                img['width'] = int(elem.text)
+            if 'height' in elem.tag:
+                img['height'] = int(elem.text)
+            if 'object' in elem.tag or 'part' in elem.tag:
+                obj = {}
+                
+                for attr in list(elem):
+                    if 'name' in attr.tag:
+                        obj['name'] = attr.text
 
-                            if obj['name'] in seen_labels:
-                                seen_labels[obj['name']] += 1
-                            else:
-                                seen_labels[obj['name']] = 1
+                        if obj['name'] in seen_labels:
+                            seen_labels[obj['name']] += 1
+                        else:
+                            seen_labels[obj['name']] = 1
+                        
+                        if len(labels) > 0 and obj['name'] not in labels:
+                            break
+                        else:
+                            img['object'] += [obj]
                             
-                            if len(labels) > 0 and obj['name'] not in labels:
-                                break
-                            else:
-                                img['object'] += [obj]
-                                
-                        if 'bndbox' in attr.tag:
-                            for dim in list(attr):
-                                if 'xmin' in dim.tag:
-                                    obj['xmin'] = int(round(float(dim.text)))
-                                if 'ymin' in dim.tag:
-                                    obj['ymin'] = int(round(float(dim.text)))
-                                if 'xmax' in dim.tag:
-                                    obj['xmax'] = int(round(float(dim.text)))
-                                if 'ymax' in dim.tag:
-                                    obj['ymax'] = int(round(float(dim.text)))
+                    if 'bndbox' in attr.tag:
+                        for dim in list(attr):
+                            if 'xmin' in dim.tag:
+                                obj['xmin'] = int(round(float(dim.text)))
+                            if 'ymin' in dim.tag:
+                                obj['ymin'] = int(round(float(dim.text)))
+                            if 'xmax' in dim.tag:
+                                obj['xmax'] = int(round(float(dim.text)))
+                            if 'ymax' in dim.tag:
+                                obj['ymax'] = int(round(float(dim.text)))
 
-            if len(img['object']) > 0:
-                all_imgs += [img]
+        if len(img['object']) > 0:
+            all_imgs += [img]
                         
     return all_imgs, seen_labels
 
