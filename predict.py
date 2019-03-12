603f8b01fd86203d347ef553bb72656302537e49
Andrade Rodrigo (C/IDI-LA) <Rodrigo.Andrade2@br.bosch.com>
2019-03-12 08:13:48 -0300

    fix imports

--- predict.py
+++ predict.py
@@ -1,19 +1,17 @@
 #! /usr/bin/env python3
+from keras_yolov2.utils import draw_boxes, get_session
+from keras_yolov2.frontend import YOLO
+from keras_yolov2.utils import list_images
 from tqdm import tqdm
-from utils import draw_boxes, get_session
-from frontend import YOLO
-from utils import list_images
-import tensorflow as tf
 import numpy as np
 import argparse
-import os
-import cv2
 import keras
 import json
+import cv2
+import os
 
-
-os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-os.environ["CUDA_VISIBLE_DEVICES"]="0"
+os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 argparser = argparse.ArgumentParser(
     description='Train and validate YOLO_v2 model on any dataset')
@@ -35,10 +33,11 @@ argparser.add_argument(
     '--input',
     help='path to an image or an video (mp4 format)')
 
+
 def _main_(args):
-    config_path  = args.conf
+    config_path = args.conf
     weights_path = args.weights
-    image_path   = args.input
+    image_path = args.input
 
     keras.backend.tensorflow_backend.set_session(get_session())
 
@@ -48,26 +47,26 @@ def _main_(args):
     if weights_path == '':
         weights_path = config['train']['pretrained_weights"']
 
-    ###############################
+    ###################
     #   Make the model 
-    ###############################
+    ###################
 
-    yolo = YOLO(backend             = config['model']['backend'],
-                input_size          = (config['model']['input_size_h'],config['model']['input_size_w']), 
-                labels              = config['model']['labels'], 
-                max_box_per_image   = config['model']['max_box_per_image'],
-                anchors             = config['model']['anchors'],
-                gray_mode           = config['model']['gray_mode'])
+    yolo = YOLO(backend=config['model']['backend'],
+                input_size=(config['model']['input_size_h'], config['model']['input_size_w']),
+                labels=config['model']['labels'],
+                max_box_per_image=config['model']['max_box_per_image'],
+                anchors=config['model']['anchors'],
+                gray_mode=config['model']['gray_mode'])
 
-    ###############################
+    #########################
     #   Load trained weights
-    ###############################    
+    #########################
 
     yolo.load_weights(weights_path)
 
-    ###############################
-    #   Predict bounding boxes 
-    ###############################
+    ###########################
+    #   Predict bounding boxes
+    ###########################
 
     if image_path[-4:] == '.mp4':
         video_out = image_path[:-4] + '_detected' + image_path[-4:]
@@ -78,11 +77,11 @@ def _main_(args):
         frame_w = int(video_reader.get(cv2.CAP_PROP_FRAME_WIDTH))
 
         video_writer = cv2.VideoWriter(video_out,
-                               cv2.VideoWriter_fourcc(*'MPEG'), 
-                               50.0, 
-                               (frame_w, frame_h))
+                                       cv2.VideoWriter_fourcc(*'MPEG'),
+                                       50.0,
+                                       (frame_w, frame_h))
 
-        for i in tqdm(range(nb_frames)):
+        for _ in tqdm(range(nb_frames)):
             _, image = video_reader.read()
             
             boxes = yolo.predict(image)
@@ -113,6 +112,7 @@ def _main_(args):
                 fname = os.path.basename(fname)
                 cv2.imwrite(os.path.join(image_path, "detected", fname), image)
 
+
 if __name__ == '__main__':
-    args = argparser.parse_args()
-    _main_(args)
+    _args = argparser.parse_args()
+    _main_(_args)
