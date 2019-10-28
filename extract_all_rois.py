d377eb75d782a604a5a9739f141c567a8bf997ef
Andrade Rodrigo (C/IDI-LA) <Rodrigo.Andrade2@br.bosch.com>
2019-10-28 08:40:42 -0300

    pep8

--- extract_all_rois.py
+++ extract_all_rois.py
@@ -1,4 +1,3 @@
-
 from preprocessing import parse_annotation_xml, parse_annotation_csv
 from tqdm import tqdm
 import numpy as np
@@ -17,10 +16,11 @@ argparser.add_argument(
     default='config.json',
     help='path to configuration file')
 
+
 def _main_(args):
     config_path = args.conf
-    
-    with open(config_path) as config_buffer:    
+
+    with open(config_path) as config_buffer:
         config = json.loads(config_buffer.read())
 
     ###############################
@@ -29,15 +29,15 @@ def _main_(args):
 
     if config['parser_annotation_type'] == 'xml':
         # parse annotations of the training set
-        train_imgs, train_labels = parse_annotation_xml(config['train']['train_annot_folder'], 
-                                                    config['train']['train_image_folder'], 
-                                                    config['model']['labels'])
+        train_imgs, train_labels = parse_annotation_xml(config['train']['train_annot_folder'],
+                                                        config['train']['train_image_folder'],
+                                                        config['model']['labels'])
 
         # parse annotations of the validation set, if any, otherwise split the training set
         if os.path.exists(config['valid']['valid_annot_folder']):
-            valid_imgs, valid_labels = parse_annotation_xml(config['valid']['valid_annot_folder'], 
-                                                        config['valid']['valid_image_folder'], 
-                                                        config['model']['labels'])
+            valid_imgs, valid_labels = parse_annotation_xml(config['valid']['valid_annot_folder'],
+                                                            config['valid']['valid_image_folder'],
+                                                            config['model']['labels'])
             split = False
         else:
             split = True
@@ -50,16 +50,17 @@ def _main_(args):
         # parse annotations of the validation set, if any, otherwise split the training set
         if os.path.exists(config['valid']['valid_csv_file']):
             valid_imgs, valid_labels = parse_annotation_csv(config['valid']['valid_csv_file'],
-                                                        config['model']['labels'],
-                                                        config['valid']['valid_csv_base_path'])
+                                                            config['model']['labels'],
+                                                            config['valid']['valid_csv_base_path'])
             split = False
         else:
             split = True
     else:
-        raise ValueError("'parser_annotations_type' must be 'xml' or 'csv' not {}.".format(config['parser_annotations_type']))
-    
+        raise ValueError(
+            "'parser_annotations_type' must be 'xml' or 'csv' not {}.".format(config['parser_annotations_type']))
+
     if split:
-        train_valid_split = int(0.8*len(train_imgs))
+        train_valid_split = int(0.8 * len(train_imgs))
         np.random.shuffle(train_imgs)
 
         valid_imgs = train_imgs[train_valid_split:]
@@ -70,7 +71,7 @@ def _main_(args):
 
         print('Seen labels:\t', train_labels)
         print('Given labels:\t', config['model']['labels'])
-        print('Overlap labels:\t', overlap_labels)           
+        print('Overlap labels:\t', overlap_labels)
 
         if len(overlap_labels) < len(config['model']['labels']):
             print('Some labels have no annotations! Please revise the list of labels in the config.json file!')
@@ -79,7 +80,7 @@ def _main_(args):
         print('No labels are provided. Saving on all seen labels.')
         config['model']['labels'] = train_labels.keys()
         with open("labels.json", 'w') as outfile:
-            json.dump({"labels" : list(train_labels.keys())},outfile)
+            json.dump({"labels": list(train_labels.keys())}, outfile)
 
     if os.path.isdir("./roi_dataset"):
         print("roi_dataset already exists, please move or delete it first.")
@@ -88,24 +89,24 @@ def _main_(args):
         os.mkdir("roi_dataset")
         os.mkdir("roi_dataset/train")
         os.mkdir("roi_dataset/val")
-        
+
         all_imgs = [train_imgs, valid_imgs]
-        for j,folder_name in enumerate(["train", "val"]):
+        for j, folder_name in enumerate(["train", "val"]):
             print("generating", folder_name)
             for img in tqdm(all_imgs[j]):
                 image = cv2.imread(img['filename'])
-                for i,obj in enumerate(img['object']):
+                for i, obj in enumerate(img['object']):
                     xmin = obj['xmin']
                     ymin = obj['ymin']
                     xmax = obj['xmax']
                     ymax = obj['ymax']
                     name = obj['name']
-                    if not os.path.isdir("roi_dataset/{}/{}".format(folder_name, name)): 
+                    if not os.path.isdir("roi_dataset/{}/{}".format(folder_name, name)):
                         os.mkdir("roi_dataset/{}/{}".format(folder_name, name))
                     roi = image[ymin:ymax, xmin:xmax]
                     base_name = os.path.basename(img['filename'])
                     base_name, ext = os.path.splitext(base_name)
-                    cv2.imwrite("roi_dataset/{}/{}/{}_{}_{}.jpg".format(folder_name,name,name, base_name, i), roi)
+                    cv2.imwrite("roi_dataset/{}/{}/{}_{}_{}.jpg".format(folder_name, name, name, base_name, i), roi)
 
 
 if __name__ == "__main__":
