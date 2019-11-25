1abda2829167c93a78cc1c77d18419390f5ceac8
Rodrigo Meira de Andrade <rodrigormda@hotmail.com>
2019-11-24 22:14:18 -0300

    heavier augmentation

--- keras_yolov2/preprocessing.py
+++ keras_yolov2/preprocessing.py
@@ -1,26 +1,28 @@
-from .utils import BoundBox, bbox_iou
+import copy
+import os
+import xml.etree.ElementTree as et
+
+import cv2
+import numpy as np
 from imgaug import augmenters as iaa
 from imgaug.augmentables import BoundingBox, BoundingBoxesOnImage
 from keras.utils import Sequence
 from tqdm import tqdm
-import xml.etree.ElementTree as et
-import numpy as np
-import copy
-import cv2
-import os
+
+from .utils import BoundBox, bbox_iou
 
 
 def parse_annotation_xml(ann_dir, img_dir, labels=[]):
     # This parser is utilized on VOC dataset
     all_imgs = []
     seen_labels = {}
-    
+
     ann_files = os.listdir(ann_dir)
     for ann in tqdm(sorted(ann_files)):
         img = {'object': []}
 
         tree = et.parse(os.path.join(ann_dir, ann))
-        
+
         for elem in tree.iter():
             if 'filename' in elem.tag:
                 img['filename'] = os.path.join(img_dir, elem.text)
@@ -30,7 +32,7 @@ def parse_annotation_xml(ann_dir, img_dir, labels=[]):
                 img['height'] = int(elem.text)
             if 'object' in elem.tag or 'part' in elem.tag:
                 obj = {}
-                
+
                 for attr in list(elem):
                     if 'name' in attr.tag:
                         obj['name'] = attr.text
@@ -39,12 +41,12 @@ def parse_annotation_xml(ann_dir, img_dir, labels=[]):
                             seen_labels[obj['name']] += 1
                         else:
                             seen_labels[obj['name']] = 1
-                        
+
                         if len(labels) > 0 and obj['name'] not in labels:
                             break
                         else:
                             img['object'] += [obj]
-                            
+
                     if 'bndbox' in attr.tag:
                         for dim in list(attr):
                             if 'xmin' in dim.tag:
@@ -58,7 +60,7 @@ def parse_annotation_xml(ann_dir, img_dir, labels=[]):
 
         if len(img['object']) > 0:
             all_imgs += [img]
-                        
+
     return all_imgs, seen_labels
 
 
@@ -80,7 +82,7 @@ def parse_annotation_csv(csv_file, labels=[], base_path=""):
             try:
                 fname, xmin, ymin, xmax, ymax, obj_name = line.strip().split(",")
                 fname = os.path.join(base_path, fname)
-                
+
                 image = cv2.imread(fname)
                 height, width, _ = image.shape
 
@@ -121,7 +123,7 @@ def parse_annotation_csv(csv_file, labels=[], base_path=""):
                     seen_labels[obj_name] += 1
 
             except:
-                print("Exception occured at line {} from {}".format(i+1, csv_file))
+                print("Exception occured at line {} from {}".format(i + 1, csv_file))
                 raise
     return all_imgs, seen_labels
 
@@ -138,7 +140,7 @@ class BatchGenerator(Sequence):
         self._callback = callback
 
         self._anchors = [BoundBox(0, 0, config['ANCHORS'][2 * i], config['ANCHORS'][2 * i + 1])
-                         for i in range(int(len(config['ANCHORS'])//2))]
+                         for i in range(int(len(config['ANCHORS']) // 2))]
 
         # augmentors by https://github.com/aleju/imgaug
         sometimes = lambda aug: iaa.Sometimes(0.5, aug)
@@ -150,14 +152,14 @@ class BatchGenerator(Sequence):
         self._aug_pipe = iaa.Sequential(
             [
                 # apply the following augmenters to most images
-                iaa.Fliplr(0.5), # horizontally flip 50% of all images
-                iaa.Flipud(0.2), # vertically flip 20% of all images
+                iaa.Fliplr(0.5),  # horizontally flip 50% of all images
+                iaa.Flipud(0.2),  # vertically flip 20% of all images
                 # sometimes(iaa.Crop(percent=(0, 0.1))), # crop images by 0-10% of their height/width
                 sometimes(iaa.Affine(
-                    # scale={"x": (0.8, 1.2), "y": (0.8, 1.2)}, # scale images to 80-120% of their size, per axis
-                    # translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)}, # translate by -20 to +20 percent
-                    # rotate=(-5, 5), # rotate by -45 to +45 degrees
-                    # shear=(-5, 5), # shear by -16 to +16 degrees
+                    scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},  # scale images to 80-120% of their size, per axis
+                    translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},  # translate by -20 to +20 percent
+                    rotate=(-5, 5),  # rotate by -45 to +45 degrees
+                    shear=(-5, 5),  # shear by -16 to +16 degrees
                     # order=[0, 1], # use nearest neighbour or bilinear interpolation (fast)
                     # cval=(0, 255), # if mode is constant, use a cval between 0 and 255
                     # mode=ia.ALL # use any of scikit-image's warping modes (see 2nd image from the top for examples)
@@ -165,32 +167,35 @@ class BatchGenerator(Sequence):
                 # execute 0 to 5 of the following (less important) augmenters per image
                 # don't execute all of them, as that would often be way too strong
                 iaa.SomeOf((0, 5),
-                    [
-                        # sometimes(iaa.Superpixels(p_replace=(0, 1.0), n_segments=(20, 200))),
-                        iaa.OneOf([
-                            iaa.GaussianBlur((0, 3.0)),  # blur images with a sigma between 0 and 3.0
-                            iaa.AverageBlur(k=(2, 7)),  # blur image using local means (kernel sizes between 2 and 7)
-                            iaa.MedianBlur(k=(3, 11)),  # blur image using local medians (kernel sizes between 2 and 7)
-                        ]),
-                        iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)),  # sharpen images
-                        # iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)), # emboss images
-                        # search either for all edges or for directed edges
-                        # sometimes(iaa.OneOf([
-                        #    iaa.EdgeDetect(alpha=(0, 0.7)),
-                        #    iaa.DirectedEdgeDetect(alpha=(0, 0.7), direction=(0.0, 1.0)),
-                        # ])),
-                        iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255), per_channel=0.5),  # add gaussian noise
-                        iaa.OneOf([
-                            iaa.Dropout((0.01, 0.1), per_channel=0.5),  # randomly remove up to 10% of the pixels
-                            # iaa.CoarseDropout((0.03, 0.15), size_percent=(0.02, 0.05), per_channel=0.2),
-                        ]),
-                        # iaa.Invert(0.05, per_channel=True), # invert color channels
-                        iaa.Add((-10, 10), per_channel=0.5),  # change brightness of images
-                        iaa.Multiply((0.5, 1.5), per_channel=0.5),  # change brightness of images
-                        iaa.ContrastNormalization((0.5, 2.0), per_channel=0.5),  # improve or worsen the contrast
-                    ],
-                    random_order=True
-                )
+                           [
+                               # sometimes(iaa.Superpixels(p_replace=(0, 1.0), n_segments=(20, 200))),
+                               iaa.OneOf([
+                                   iaa.GaussianBlur((0, 3.0)),  # blur images with a sigma between 0 and 3.0
+                                   iaa.AverageBlur(k=(2, 7)),
+                                   # blur image using local means (kernel sizes between 2 and 7)
+                                   iaa.MedianBlur(k=(3, 11)),
+                                   # blur image using local medians (kernel sizes between 2 and 7)
+                               ]),
+                               iaa.Sharpen(alpha=(0, 1.0), lightness=(0.75, 1.5)),  # sharpen images
+                               iaa.Emboss(alpha=(0, 1.0), strength=(0, 2.0)), # emboss images
+                               # search either for all edges or for directed edges
+                               # sometimes(iaa.OneOf([
+                               #    iaa.EdgeDetect(alpha=(0, 0.7)),
+                               #    iaa.DirectedEdgeDetect(alpha=(0, 0.7), direction=(0.0, 1.0)),
+                               # ])),
+                               iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05 * 255), per_channel=0.5),
+                               # add gaussian noise
+                               iaa.OneOf([
+                                   iaa.Dropout((0.01, 0.1), per_channel=0.5),  # randomly remove up to 10% of the pixels
+                                   # iaa.CoarseDropout((0.03, 0.15), size_percent=(0.02, 0.05), per_channel=0.2),
+                               ]),
+                               iaa.Invert(0.05, per_channel=True), # invert color channels
+                               iaa.Add((-10, 10), per_channel=0.5),  # change brightness of images
+                               iaa.Multiply((0.5, 1.5), per_channel=0.5),  # change brightness of images
+                               iaa.ContrastNormalization((0.5, 2.0), per_channel=0.5),  # improve or worsen the contrast
+                           ],
+                           random_order=True
+                           )
             ],
             random_order=True
         )
@@ -230,8 +235,8 @@ class BatchGenerator(Sequence):
         return image
 
     def __getitem__(self, idx):
-        l_bound = idx*self._config['BATCH_SIZE']
-        r_bound = (idx+1)*self._config['BATCH_SIZE']
+        l_bound = idx * self._config['BATCH_SIZE']
+        r_bound = (idx + 1) * self._config['BATCH_SIZE']
 
         if r_bound > len(self._images):
             r_bound = len(self._images)
@@ -252,9 +257,9 @@ class BatchGenerator(Sequence):
 
             for obj in all_objs:
                 if obj['xmax'] > obj['xmin'] and obj['ymax'] > obj['ymin'] and obj['name'] in self._config['LABELS']:
-                    center_x = .5*(obj['xmin'] + obj['xmax'])
+                    center_x = .5 * (obj['xmin'] + obj['xmax'])
                     center_x = center_x / (float(self._config['IMAGE_W']) / self._config['GRID_W'])
-                    center_y = .5*(obj['ymin'] + obj['ymax'])
+                    center_y = .5 * (obj['ymin'] + obj['ymax'])
                     center_y = center_y / (float(self._config['IMAGE_H']) / self._config['GRID_H'])
 
                     grid_x = int(np.floor(center_x))
@@ -262,26 +267,28 @@ class BatchGenerator(Sequence):
 
                     if grid_x < self._config['GRID_W'] and grid_y < self._config['GRID_H']:
                         obj_indx = self._config['LABELS'].index(obj['name'])
-                        
-                        center_w = (obj['xmax'] - obj['xmin'])/(float(self._config['IMAGE_W'])/self._config['GRID_W'])
-                        center_h = (obj['ymax'] - obj['ymin'])/(float(self._config['IMAGE_H'])/self._config['GRID_H'])
-                        
+
+                        center_w = (obj['xmax'] - obj['xmin']) / (
+                                    float(self._config['IMAGE_W']) / self._config['GRID_W'])
+                        center_h = (obj['ymax'] - obj['ymin']) / (
+                                    float(self._config['IMAGE_H']) / self._config['GRID_H'])
+
                         box = [center_x, center_y, center_w, center_h]
 
                         # find the anchor that best predicts this box
                         best_anchor = -1
                         max_iou = -1
-                        
+
                         shifted_box = BoundBox(0, 0, center_w, center_h)
-                        
+
                         for i in range(len(self._anchors)):
                             anchor = self._anchors[i]
                             iou = bbox_iou(shifted_box, anchor)
-                            
+
                             if max_iou < iou:
                                 best_anchor = i
                                 max_iou = iou
-                                
+
                         # assign ground truth x, y, w, h, confidence and class probs to y_batch
                         y_batch[instance_count, grid_y, grid_x, best_anchor, 0:4] = box
                         y_batch[instance_count, grid_y, grid_x, best_anchor, 4] = 1.
@@ -296,12 +303,12 @@ class BatchGenerator(Sequence):
                     if obj['xmax'] > obj['xmin'] and obj['ymax'] > obj['ymin']:
                         cv2.rectangle(img[..., ::-1], (obj['xmin'], obj['ymin']), (obj['xmax'], obj['ymax']),
                                       (255, 0, 0), 3)
-                        cv2.putText(img[..., ::-1], obj['name'], (obj['xmin']+2, obj['ymin']+12), 0,
-                                    1.2e-3*img.shape[0], (0, 255, 0), 2)
-                        
+                        cv2.putText(img[..., ::-1], obj['name'], (obj['xmin'] + 2, obj['ymin'] + 12), 0,
+                                    1.2e-3 * img.shape[0], (0, 255, 0), 2)
+
                 x_batch[instance_count] = img
             # increase instance counter in current batch
-            instance_count += 1  
+            instance_count += 1
 
         return x_batch, y_batch
 
@@ -345,7 +352,7 @@ class BatchGenerator(Sequence):
                     all_objs[i]['xmax'] = bbs.bounding_boxes[i].x2
                     all_objs[i]['ymin'] = bbs.bounding_boxes[i].y1
                     all_objs[i]['ymax'] = bbs.bounding_boxes[i].y2
-            
+
         # resize the image to standard size
         image = cv2.resize(image, (self._config['IMAGE_W'], self._config['IMAGE_H']))
         if self._config['IMAGE_C'] == 1:
