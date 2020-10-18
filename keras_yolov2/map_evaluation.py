1330784a2c02d28b706cd2b78b9d847041b58ef4
Rodrigo Andrade <rodrigormda@hotmail.com>
2020-10-18 20:22:22 -0300

    fix map evaluation callback

--- keras_yolov2/map_evaluation.py
+++ keras_yolov2/map_evaluation.py
@@ -1,7 +1,7 @@
 import numpy as np
 import tensorflow as tf
 from tensorflow import keras
-
+from tensorflow.python.ops import summary_ops_v2
 from .utils import compute_overlap, compute_ap
 
 
@@ -60,12 +60,11 @@ class MapEvaluation(keras.callbacks.Callback):
             else:
                 print("mAP did not improve from {}.".format(self.bestMap))
 
-            if self._tensorboard is not None and self._tensorboard.writer is not None:
-                summary = tf.Summary()
-                summary_value = summary.value.add()
-                summary_value.simple_value = _map
-                summary_value.tag = "val_mAP"
-                self._tensorboard.writer.add_summary(summary, epoch)
+            if self._tensorboard is not None:
+                with summary_ops_v2.always_record_summaries():
+                    with self._tensorboard._val_writer.as_default():
+                        name = "mAP"  # Remove 'val_' prefix.
+                        summary_ops_v2.scalar('epoch_' + name, _map, step=epoch)
 
     def evaluate_map(self):
         average_precisions = self._calc_avg_precisions()
